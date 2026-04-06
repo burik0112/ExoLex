ab46d59ae08be3f98a8bd7a4f1729f88bd7f2479
Пользователь <burhonabdullaev727@gmail.com>
2026-04-06 11:27:04 +0500

    wsgi

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d+*#7bjm^z!dc(zm4u+64#&sz2$)*h2n)m#%xgzca^yd0vq3x0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['burik.pythonanywhere.com', 'localhost', '127.0.0.1']
 
--- config/wsgi.py
+++ config/wsgi.py
@@ -1,11 +1,16 @@
 import os
 import sys
 
+# 1. Loyihang joylashgan papka manzili
+# Rasmingda loyiha nomi 'Exolex' (kichik harf bilan)
 path = '/home/Burik/Exolex'
 if path not in sys.path:
     sys.path.append(path)
 
+# 2. Django settings fayliga yo'l
+# Rasmingda settings.py fayli 'config' degan papkada turibdi
 os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings'
 
+# 3. WSGI dasturini ishga tushirish
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()

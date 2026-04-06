ba474714d27301e424905417861396a5f14ffc15
Пользователь <burhonabdullaev727@gmail.com>
2026-04-06 10:33:05 +0500

    data

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,15 +1,15 @@
 import os
 import sys
 
-# 1. Loyihang joylashgan papka (Katta-kichik harfiga qara!)
+# 1. Loyihang turgan aniq yo'l
 path = '/home/Burik/Exolex'
 if path not in sys.path:
     sys.path.append(path)
 
-# 2. Settings fayli qayerda?
-# Agar settings.py fayling 'config' degan papkada bo'lsa:
+# 2. Settings qayerda?
+# Agar settings.py fayling 'config' papkasida bo'lsa:
 os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings'
+# AGAR 'config' papkasi bo'lmasa, unda 'Exolex.settings' deb yozib ko'r.
 
-# 3. Djangoni ishga tushirish
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()

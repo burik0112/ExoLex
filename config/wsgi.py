98af0c521d88cbb5625c0174a6953c9cf7d11228
Пользователь <burhonabdullaev727@gmail.com>
2026-04-06 09:46:06 +0500

    add

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,20 +1,13 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# Loyiha yo'li
+path = '/home/burik0112/ExoLex'
+if path not in sys.path:
+    sys.path.append(path)
+
+# Settings fayli manzili. Loyiha papkang 'config' bo'lsa:
+os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-from cron import start_ping
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
 application = get_wsgi_application()
-
-start_ping()

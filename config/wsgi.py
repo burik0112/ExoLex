4f0bd2904d2da2e0e6c6e286b6b9cc53d8431d66
Пользователь <burhonabdullaev727@gmail.com>
2026-04-06 10:25:33 +0500

    wsg

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,13 +1,15 @@
 import os
 import sys
 
-# Loyiha yo'li
+# 1. Loyihang joylashgan papka (Katta-kichik harfiga qara!)
 path = '/home/Burik/Exolex'
 if path not in sys.path:
     sys.path.append(path)
 
-# config - bu settings.py fayli turgan papka nomi
+# 2. Settings fayli qayerda?
+# Agar settings.py fayling 'config' degan papkada bo'lsa:
 os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings'
 
+# 3. Djangoni ishga tushirish
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
